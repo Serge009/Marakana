3f40a3191cd842f7f880d154c9f917dfa7d35e64
SERGE <serge.naumovych@gmail.com>
2014-08-31 19:57:44 +0300

    Initial commit

---
+++ src/main/java/com/marakana/contacts/Contact.java
@@ -0,0 +1,37 @@
+package com.marakana.contacts;
+
+/**
+ * Created by SERGE on 31.08.2014.
+ */
+public class Contact {
+
+    private Long id;
+
+    private String name;
+
+    private Long addressId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+}
